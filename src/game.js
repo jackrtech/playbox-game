2949156223284cf62b664a97fcca45656d6e23a8
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2026-01-23 15:08:14 +0000

    Added responsive background scaling with fitCover

--- src/game.js
+++ src/game.js
@@ -5,8 +5,29 @@ export async function startGame(app) {
 
   const bg = PIXI.Sprite.from("bg");
   bg.anchor.set(0.5);
-  bg.x = app.screen.width / 2;
-  bg.y = app.screen.height / 2;
-
   app.stage.addChild(bg);
+
+  function fitCover(sprite, w, h) {
+    const sw = sprite.texture.width;
+    const sh = sprite.texture.height; // get original image dimensions
+    const scale = Math.max(w / sw, h / sh); // calculate scale needed
+    sprite.scale.set(scale);
+
+    // w / sw = scale needed to fit image width
+    // h / sh = scale needed to fit image height
+  } 
+
+  function layout() {
+    const w = app.renderer.width;
+    const h = app.renderer.height;
+    bg.x = w / 2;
+    bg.y = h / 2;
+    fitCover(bg, w, h);
+  }
+
+  // call layout function once
+  layout();
+
+  // recall layout again whenever Pixi resizes
+  window.addEventListener("resize", layout);
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { startGame } from "./game.js";
 
 const app = new PIXI.Application({
-  width: 720,
-  height: 1280,
   backgroundColor: 0x1c1c1c,
   antialias: true,
+  resizeTo: document.querySelector("#game"),
 });
 
 document.querySelector("#game").appendChild(app.view);
