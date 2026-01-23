0c1ec271554fbf1a3787e09c594b32dc941e9b8a
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2026-01-23 19:00:40 +0000

    Added player sprite with scale &  position in layout()

--- src/game.js
+++ src/game.js
@@ -7,6 +7,10 @@ export async function startGame(app) {
   bg.anchor.set(0.5);
   app.stage.addChild(bg);
 
+  const player = PIXI.Sprite.from("sheetPlayer");
+  player.anchor.set(0.5, 1); // center horizontally, bottom vertically
+  app.stage.addChild(player);
+
   function fitCover(sprite, w, h) {
     const sw = sprite.texture.width;
     const sh = sprite.texture.height; // get original image dimensions
@@ -20,9 +24,17 @@ export async function startGame(app) {
   function layout() {
     const w = app.renderer.width;
     const h = app.renderer.height;
+
+    // background
     bg.x = w / 2;
     bg.y = h / 2;
     fitCover(bg, w, h);
+
+    // player
+    player.x = w *0.25 //player 25% from left
+    player.y = h *0.90
+
+    player.scale.set(0.4) 
   }
 
   // call layout function once
